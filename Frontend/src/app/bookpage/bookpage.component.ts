33a75e5a7f3ab81bb18544f897dd4ca1ff305b13
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-07-18 23:44:11 +0530

    Add files via upload

---
+++ Frontend/src/app/bookpage/bookpage.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { NgForm } from '@angular/forms';
+import { Book } from '../shared/book.model';
+declare var M: any;
+
+@Component({
+  selector: 'app-bookpage',
+  templateUrl: './bookpage.component.html',
+  styleUrls: ['./bookpage.component.css'],
+  providers:[BookService]
+})
+export class BookpageComponent implements OnInit {
+  
+
+  constructor(public bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.resetForm();
+    this.refreshBookList();
+  }
+  resetForm(form?: NgForm) {
+    if (form)
+      form.reset();
+    this.bookService.selectedBook = {
+      _id: '',
+      title: '',
+      author: '',
+      about: ''
+      
+    }
+  }
+  onSubmit(form: NgForm) {
+    if (form.value._id == "") {
+      this.bookService.postBook(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshBookList();
+        M.toast({ html: 'Saved successfully', classes: 'rounded' });
+      });
+    }
+    else {
+      this.bookService.putBook(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshBookList();
+        M.toast({ html: 'Updated successfully', classes: 'rounded' });
+      });
+    }
+  }
+  
+  refreshBookList() {
+    this.bookService.getBookList().subscribe((res) => {
+      this.bookService.books = res as Book[];
+    });
+  }
+
+  onEdit(emp: Book) {
+    this.bookService.selectedBook = emp;
+  }
+
+  onDelete(_id: string, form: NgForm) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.bookService.deleteBook(_id).subscribe((res) => {
+        this.refreshBookList();
+        this.resetForm(form);
+        M.toast({ html: 'Deleted successfully', classes: 'rounded' });
+      });
+    }
+  }
+}
